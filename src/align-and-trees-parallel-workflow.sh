f1156e45a0066072624d2e512f44eba10c8e03c4
nylander <Johan.Nylander@nrm.se>
2022-05-31 18:23:12 +0200

    rewrite

--- src/align-and-trees-parallel-workflow.sh
+++ src/align-and-trees-parallel-workflow.sh
@@ -72,7 +72,7 @@ phylip2fasta="phylip2fasta.pl"
 
 prog_exists() {
     if ! command -v "$1" &> /dev/null ; then
-        echo "## ALIGN-AND-TREES-WORKFLOW: ERROR: $1 could not be found"
+        echo -e "\n## ALIGN-AND-TREES-WORKFLOW: ERROR: $1 could not be found"
         exit 1
     fi
 }
@@ -147,37 +147,37 @@ fi
 
 if [ -d "${unaligned}" ] ; then
     if [ "$(find "${unaligned}" -name '*.fas' | wc -l)" -gt 1 ] ; then
-        echo "## ALIGN-AND-TREES-WORKFLOW: Found .fas files in folder ${unaligned}"
+        echo -e "\n## ALIGN-AND-TREES-WORKFLOW: Found .fas files in folder ${unaligned}"
     else
-        echo "## ALIGN-AND-TREES-WORKFLOW: ERROR: Could not find .fas files in folder ${unaligned}"
+        echo -e "\n## ALIGN-AND-TREES-WORKFLOW: ERROR: Could not find .fas files in folder ${unaligned}"
         exit 1
     fi
 else
-    echo "## ALIGN-AND-TREES-WORKFLOW: ERROR: folder ${unaligned} can not be found"
+    echo -e "\n## ALIGN-AND-TREES-WORKFLOW: ERROR: folder ${unaligned} can not be found"
     exit 1
 fi
 
 if [ -d "${runfolder}" ]; then
-    echo "## ALIGN-AND-TREES-WORKFLOW: ERROR: folder ${runfolder} exists"
+    echo -e "\n## ALIGN-AND-TREES-WORKFLOW: ERROR: folder ${runfolder} exists"
     exit 1
 else
     mkdir -p "${runfolder}"
-    echo "## ALIGN-AND-TREES-WORKFLOW: Created output folder ${runfolder}"
+    echo -e "\n## ALIGN-AND-TREES-WORKFLOW: Created output folder ${runfolder}"
 fi
 
 if [ "${dflag}" ] ; then
     datatype="${dval}"
-    # Need to check if 'nt' or 'aa'
+    ## TODO: Need to check if 'nt' or 'aa'
 fi
 
 if [ "${tflag}" ] ; then
     threads="${tval}"
-    ncores="${threads}" # For now
+    ncores="${threads}" # TODO: differentiate these variables
 fi
 
 if [ "${mflag}" ] ; then
     modeltestcriterion="${mval}"
-    # Need to check if 'BIC', 'AIC', or 'AICC'(?)
+    ## TODO: Need to check if 'BIC', 'AIC', or 'AICC'(?)
 fi
 
 ## Needed for some bash functions
@@ -213,110 +213,150 @@ checkNtaxaInFasta() {
 }
 export -f checkNtaxaInFasta
 
+######################################################################################
 ## Step 1. Alignments with mafft
+## Input: inputfolder/*.fas
+## Output: 1_align/1.1_mafft/*.mafft.ali
 ## TODO: redirect stderr?
-echo "## ALIGN-AND-TREES-WORKFLOW: Align with ${aligner}"
-mkdir -p "${runfolder}/align/${aligner}"
+echo -e "\n## ALIGN-AND-TREES-WORKFLOW: Align with ${aligner}"
+mkdir -p "${runfolder}/1_align/1.1_${aligner}"
 find "${unaligned}" -type f -name '*.fas' | \
-    parallel ''"${alignerbin}"' '"${alignerbinopts}"' {} > '"${runfolder}"'/align/'"${aligner}"'/{/.}.'"${aligner}"'.ali'
-    #parallel ''"${alignerbin}"' '"${alignerbinopts}"' {} > '"${runfolder}"'/align/'"${aligner}"'/{/.}.'"${aligner}"'.ali 2> /dev/null'
+    parallel ''"${alignerbin}"' '"${alignerbinopts}"' {} > '"${runfolder}"'/1_align/1.1_'"${aligner}"'/{/.}.'"${aligner}"'.ali'
 
+
+######################################################################################
 ## Step 2. Check alignments with raxml-ng
+## Input: 1_align/1.2_mafft_check/*.mafft.ali
+## Output: 1_align/1.2_mafft_check/*.mafft.ali.raxml.log, and, sometimes, 1_align/1.2_mafft_check/*.mafft.ali.raxml.reduced.phy
 ## TODO: redirect stderr?
-echo '## ALIGN-AND-TREES-WORKFLOW: Check alignments with raxml-ng'
-mkdir -p "${runfolder}/align/${aligner}.check"
-ln -s -f "${runfolder}/align/${aligner}"/*.ali "${runfolder}/align/${aligner}.check/"
-cd "${runfolder}/align/${aligner}.check" || exit
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Check alignments with raxml-ng'
+mkdir -p "${runfolder}/1_align/1.2_${aligner}_check"
+ln -s -f "${runfolder}/1_align/1.1_${aligner}"/*.ali "${runfolder}/1_align/1.2_${aligner}_check/"
+cd "${runfolder}/1_align/1.2_${aligner}_check" || exit
 find -L . -type f -name '*.ali' | \
     parallel ''"${raxmlng}"' --check --msa {} --threads 1 --model '"${modelforraxmltest}"' >/dev/null || true'
 
+
+######################################################################################
 ## Step 3. Find error in logs.
-## TODO: remove also the ali file!
-cd "${runfolder}/align/${aligner}.check" || exit
-echo '## ALIGN-AND-TREES-WORKFLOW: Find error in logs. If error, remove the ali file'
+## Input: 1_align/1.2_mafft.check/*.mafft.ali.raxml.log
+## Output: removes 1_align/1.2_mafft.check/*.mafft.ali if error
+## TODO: 
+cd "${runfolder}/1_align/1.2_${aligner}_check" || exit
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Find error in logs. If error, remove the ali file'
 find . -type f -name '*.log' | \
     parallel 'if grep -q "^ERROR" {} ; then echo "found error in {}"; rm -v {=s/\.raxml\.log//=} ; fi'
 
-## Step 4. If no .raxml.reduced.phy file was created, create one
-## TODO: Do not use the .reduced.phy alignments! So skip this step. We wish to use all ali files which are not removed above.
-cd "${runfolder}/align/${aligner}.check" || exit
-echo '## ALIGN-AND-TREES-WORKFLOW: If no .raxml.reduced.phy file was created, create one'
-find -L . -type f -name '*.ali' | \
-    parallel 'if [ ! -e {}.raxml.reduced.phy ] ; then '"${catfasta2phyml}"' {} 2> /dev/null > {}.raxml.reduced.phy; fi'
 
-## Step 5. Check and remove if any of the .phy files have less than 4 taxa
-## TODO: Do not use the .reduced.phy alignments! So skip this step. We wish to use all ali files which are not removed above.
-echo '## ALIGN-AND-TREES-WORKFLOW: Check and remove if any of the .reduced.phy files have less than 4 taxa'
-find "${runfolder}/align/${aligner}.check/" -type f -name '*.reduced.phy' | \
-    parallel checkNtaxaInPhylip
-
-## Step 6. Remove all .ali files in the check directory
+######################################################################################
+## Step 4. Remove all .ali files in the check directory
+## Input: folder 1_align/1.2_mafft_check
+## Output: Removes *.log *.raxml.reduced.phy
 ## TODO: we actually wish to keep and use the ali files!
-echo '## ALIGN-AND-TREES-WORKFLOW: Remove all .ali files in the check directory'
-cd "${runfolder}/align/${aligner}.check" || exit
-rm ./*.ali ./*.log
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Remove all .ali files in the check directory'
+cd "${runfolder}/1_align/1.2_${aligner}_check" || exit
+rm ./*.log ./*.raxml.reduced.phy
 
-## Step 7. Run first run of BMGE
-## TODO: Do not use the .reduced.phy alignments! Use the ali files
-echo '## ALIGN-AND-TREES-WORKFLOW: Run BMGE'
-mkdir -p "${runfolder}/align/bmge"
-cd "${runfolder}/align/bmge" || exit
-find "${runfolder}/align/${aligner}.check/" -type f -name '*.reduced.phy' | \
-    parallel 'java -jar '"${bmgejar}"' -i {} -t '"${datatypeforbmge}"' -o {/.}.bmge.phy'
 
-## Step 8. Check and remove if any of the .bmge.phy files have less than 4 taxa
-echo '## ALIGN-AND-TREES-WORKFLOW: Check and remove if any of the .bmge.phy files have less than 4 taxa'
-find "${runfolder}/align/bmge" -type f -name '*.phy' | \
-    parallel checkNtaxaInPhylip
+######################################################################################
+## Step 5. Run first run of BMGE
+## Input: 1_align/1.2_mafft_check/*.mafft.ali (symlinks)
+## Output: 1_align/1.3_mafft_check_bmge/*.bmge.ali
+## TODO:
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Run BMGE'
+mkdir -p "${runfolder}/1_align/1.3_mafft_check_bmge"
+cd "${runfolder}/1_align/1.3_mafft_check_bmge" || exit
+find -L "${runfolder}/1_align/1.2_${aligner}_check/" -type f -name '*.ali' | \
+    parallel 'java -jar '"${bmgejar}"' -i {} -t '"${datatypeforbmge}"' -of {/.}.bmge.ali'
 
-## Step 9. Run pargenes on the .bmge.phy files with fixed model
-echo '## ALIGN-AND-TREES-WORKFLOW: Run first round of pargenes (raxml-ng)'
-mkdir -p "${runfolder}/trees"
-cd "${runfolder}/trees" || exit
+
+######################################################################################
+## Step 6. Check and remove if any of the .mafft.bmge.ali files have less than 4 taxa
+## Input: 1_align/1.3_mafft_check_bmge/*.mafft.bmge.ali
+## Output: remove /1_align/1.3_mafft_check_bmge/*.mafft.bmge.ali files
+## TODO:
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Check and remove if any of the .bmge.phy files have less than 4 taxa'
+find "${runfolder}/1_align/1.3_mafft_check_bmge" -type f -name '*.ali' | \
+    parallel checkNtaxaInFasta
+
+
+######################################################################################
+## Step 7. Run pargenes on the .bmge.ali files with fixed model
+## Input: /1_align/1.3_mafft_check_bmge
+## Output: /2_trees/2.1_mafft_check_bmge_pargenes
+## TODO:
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Run first round of pargenes (raxml-ng)'
+mkdir -p "${runfolder}/2_trees"
+cd "${runfolder}/2_trees" || exit
 "${pargenes}" \
-    --alignments-dir "${runfolder}/align/bmge" \
-    --output-dir "${runfolder}/trees/pargenes-bmge" \
+    --alignments-dir "${runfolder}/1_align/1.3_mafft_check_bmge" \
+    --output-dir "${runfolder}/2_trees/2.1_mafft_check_bmge_pargenes" \
     --cores "${ncores}" \
     --datatype "${datatype}" \
     --raxml-global-parameters-string "--model ${modelforpargenesfixed}"
 
-## Step 10. Prepare input for threeshrink
-echo '## ALIGN-AND-TREES-WORKFLOW: Prepare input for threeshrink'
-mkdir -p "${runfolder}/treeshrink/input-bmge"
+
+######################################################################################
+## Step 8. Prepare input for threeshrink
+## Input: 1_align/1.3_mafft_check_bmge/*.bmge.ali
+## Output: 3_treeshrink/3.1_input-bmge/EOG7B0H2N_mafft_bmge_ali/mafft.ali
+## TODO: 
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Prepare input for threeshrink'
+mkdir -p "${runfolder}/3_treeshrink/3.1_treeshrink"
 copyAndConvert () {
-    f=$(basename "$1") # f=EOG7FC368.mafft.ali.raxml.reduced.bmge.phy
-    s="${f//\./_}"   # s=EOG7FC368_mafft_ali_raxml_reduced_bmge_phy
-    mkdir -p "${runfolder}/treeshrink/input-bmge/${s}"
-    cp "${runfolder}/trees/pargenes-bmge/mlsearch_run/results/${s}/${s}.raxml.bestTree" "${runfolder}/treeshrink/input-bmge/${s}/raxml.bestTree"
-    "${phylip2fasta}" -i "${runfolder}/align/bmge/${f}" -o "${runfolder}/treeshrink/input-bmge/${s}/${aligner}.ali"
+    f=$(basename "$1") # f=EOG7B0H2N.mafft.bmge.ali
+    s="${f//\./_}"     # s=EOG7B0H2N_mafft_bmge_ali
+    mkdir -p "${runfolder}/3_treeshrink/3.1_treeshrink/${s}"
+    ln -s "${runfolder}/2_trees/2.1_mafft_check_bmge_pargenes/mlsearch_run/results/${s}/${s}.raxml.bestTree" "${runfolder}/3_treeshrink/3.1_treeshrink/${s}/raxml.bestTree"
+    ln -s "${runfolder}/1_align/1.3_mafft_check_bmge/${f}" "${runfolder}/3_treeshrink/3.1_treeshrink/${s}/${aligner}.ali"
 }
 export -f copyAndConvert
-find "${runfolder}/align/bmge/" -type f -name '*.bmge.phy' | \
+find "${runfolder}/1_align/1.3_mafft_check_bmge/" -type f -name '*.bmge.ali' | \
     parallel copyAndConvert
 
-## Step 11. Run treeshrink
-echo '## ALIGN-AND-TREES-WORKFLOW: Run treeshrink'
+
+######################################################################################
+## Step 9. Run treeshrink
+## Input: 3_treeshrink/3.1_input-bmge
+## Output: 
+## TODO:
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Run treeshrink'
 "${treeshrink}" \
-    --indir "${runfolder}/treeshrink/input-bmge" \
+    --indir "${runfolder}/3_treeshrink/3.1_treeshrink" \
     --tree 'raxml.bestTree' \
     --alignment "${aligner}.ali"
 
-## Step 12. Check and remove if any of the output.ali files have less than 4 taxa
-echo '## ALIGN-AND-TREES-WORKFLOW: Check and remove if any of the output.ali files from treeshrink have less than 4 taxa'
-find "${runfolder}/treeshrink/input-bmge" -type f -name 'output.ali' | \
+
+######################################################################################
+## Step 10. Check and remove if any of the output.ali files have less than 4 taxa
+## Input: 3_treeshrink/3.1_input-bmge
+## Output: remove output.ali files
+## TODO:
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Check and remove if any of the output.ali files from treeshrink have less than 4 taxa'
+find "${runfolder}/3_treeshrink/3.1_treeshrink" -type f -name 'output.ali' | \
     parallel checkNtaxaInFasta
 
-## Step 13. Realign using realigner
-echo '## ALIGN-AND-TREES-WORKFLOW: Realign using realigner'
-mkdir -p "${runfolder}/treeshrink/realign-bmge"
-find "${runfolder}/treeshrink/input-bmge/" -type f -name 'output.ali' | \
-    parallel 'b=$(basename {//} .ali); '"${realigner}"' --auto --thread '"${threadsforparallel}"' <('"${fastagap}"' {}) > '"${runfolder}"'/treeshrink/realign-bmge/"${b//_/\.}.ali"'
 
-## Step 14. Run pargenes again, finish with ASTRAL
-echo '## ALIGN-AND-TREES-WORKFLOW: Run pargenes again, finish with ASTRAL'
+######################################################################################
+## Step 11. Realign using realigner
+## Input: 3_treeshrink/3.1_input-bmge/
+## Output: 1_align/1.4_mafft_check_bmge_treeshrink
+## TODO:
+echo -e '\n## ALIGN-AND-TREES-WORKFLOW: Realign after treeshrink using realigner'
+mkdir -p "${runfolder}/1_align/1.4_mafft_check_bmge_treeshrink"
+find "${runfolder}/3_treeshrink/3.1_treeshrink/" -type f -name 'output.ali' | \
+    parallel 'b=$(basename {//} .ali); '"${realigner}"' --auto --thread '"${threadsforparallel}"' <('"${fastagap}"' {}) > '"${runfolder}"'/1_align/1.4_mafft_check_bmge_treeshrink/"${b//_/\.}"'
+
+
+######################################################################################
+## Step 12. Run pargenes again, finish with ASTRAL
+## Input: 1_align/1.4_mafft_check_bmge_treeshrink/*.mafft.bmge.ali
+## Output: 2_trees/2.2_mafft_check_bmge_treeshrink_pargenes
+## TODO:
+echo -e "\n## ALIGN-AND-TREES-WORKFLOW: Run pargenes again, finish with ASTRAL"
 "${pargenes}" \
-    --alignments-dir "${runfolder}/treeshrink/realign-bmge" \
-    --output-dir "${runfolder}/trees/pargenes-bmge-treeshrink" \
+    --alignments-dir "${runfolder}/1_align/1.4_mafft_check_bmge_treeshrink" \
+    --output-dir "${runfolder}/2_trees/2.2_mafft_check_bmge_treeshrink_pargenes" \
     --cores "${ncores}" \
     --datatype "${datatype}" \
     --use-modeltest \
@@ -324,9 +364,13 @@ echo '## ALIGN-AND-TREES-WORKFLOW: Run pargenes again, finish with ASTRAL'
     --modeltest-perjob-cores "${modeltestperjobcores}" \
     --use-astral
 
+
+######################################################################################
 ## End
-echo "## ALIGN-AND-TREES-WORKFLOW: Reached end of trees script."
-echo "## ALIGN-AND-TREES-WORKFLOW: Final species tree should be in folder:"
-echo -e "## ALIGN-AND-TREES-WORKFLOW: ${runfolder}/trees/pargenes-bmge-treeshrink/astral_run"
-tree "${runfolder}/trees/pargenes-bmge-treeshrink/astral_run"
+echo -e "\n## ALIGN-AND-TREES-WORKFLOW: Reached end of trees script."
+echo -e "\n## ALIGN-AND-TREES-WORKFLOW: Final species tree should be in folder:"
+echo -e "\n## ALIGN-AND-TREES-WORKFLOW: ${runfolder}/2_trees/2.2_mafft_check_bmge_treeshrink_pargenes/astral_run"
+tree "${runfolder}/2_trees/2.2_mafft_check_bmge_treeshrink_pargenes/astral_run"
+
+exit 0
 
