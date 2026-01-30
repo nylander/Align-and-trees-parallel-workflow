b68b14446b2f3da9005ca6187232248a75a9e7ae
nylander <Johan.Nylander@nrm.se>
2026-01-30 15:18:10 +0100

    Add Pelle slurm script

--- singularity/atpw-pelle.slurm.sh
+++ singularity/atpw-pelle.slurm.sh
@@ -11,7 +11,7 @@
 # Note: one node on pelle has 48 cores (96 threads)
 
 # atpw-pelle.slurm.sh
-# Last modified: 2026-01-28 10:17:07
+# Last modified: 2026-01-30 15:17:34
 # Sign: JN
 #
 # Test by using
@@ -25,21 +25,18 @@
 # Monitor by using
 #     squeue -u $USER
 #
-# More reading: https://docs.uppmax.uu.se/cluster_guides/slurm_on_pelle/
+# More reading: https://docs.uppmax.uu.se/cluster_guides/slurm_on_pelle
 #
-# Testing
-#   $ ATPW=$HOME/bin/atpw.sif
-#   $ export ATPW
-#   $ singularity run $ATPW -h
+# Testing (JN)
 #   $ cd $HOME/run/atpw-testing
-#   $ sbatch atpw-pelle.slurm.sh fasta_files fasta_files_out
+#   $ sbatch atpw-pelle.slurm.sh 7_fasta_files 7_fasta_files_out
 
 # ------------------------------------------------------------
 ATPW="${ATPW:-$HOME/bin/atpw.sif}"
                  # ^ Edit path above to atpw
 data_type='nt'   # < Edit here if not nt input
-n_cpus="${SLURM_CPUS_PER_TASK}"  # < 96: One node on pelle
-other_options=' -i 100 ' # Other options to atpw here
+n_cpus="${SLURM_CPUS_PER_TASK}"
+other_options=' -i 1000 ' # Other options to atpw here
 # ------------------------------------------------------------
 
 start=$(date +%s)
@@ -52,27 +49,15 @@ fi
 in_folder=$1
 out_folder=$2
 
-basename_in_folder=$(basename "${in_folder}")
-tmp_in_folder="${basename_in_folder}"
-cp -r "${in_folder}" "$SNIC_TMP"
-
 if [ -d "${out_folder}" ]; then
   echo "Directory ${out_folder} exists. Exiting."
   exit
 fi
 
-if [ -d "${SNIC_TMP}${out_folder}" ]; then
-  rm -rf "${SNIC_TMP}${out_folder}"
-fi
+#singularity run "${ATPW}" -d "${data_type}" -n "${n_cpus}" "${other_options}" "${in_folder}" "${out_folder}"
 
-basename_out_folder=$(basename "${out_folder}")
-tmp_out_folder="${basename_out_folder}"
-
-cp -r "${in_folder}" "$SNIC_TMP"
-
-cd "$SNIC_TMP" || exit
-
-singularity run "${ATPW}" -d "${data_type}" -n "${n_cpus}" "${other_options}" "${tmp_in_folder}" "${tmp_out_folder}" && cp -r "${tmp_out_folder}" "$SLURM_SUBMIT_DIR/${basename_out_folder}"
+# NOTE: hard coded bootstrap!
+singularity run -B "${SNIC_TMP}" "${ATPW}" -d "${data_type}" -n "${n_cpus}" -i 1000 "${in_folder}" "${out_folder}"
 
 end=$(date +%s)
 runtime=$((end-start))
